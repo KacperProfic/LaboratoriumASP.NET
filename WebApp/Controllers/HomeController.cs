879716e2c47f539fa1436ceedc87eef14d2ed646
kacper.profic <kacper.profic@microsoft.wsei.edu.pl>
2024-10-08 18:22:51 +0200

    abc

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -13,6 +13,64 @@ public class HomeController : Controller
         _logger = logger;
     }
 
+    public IActionResult About()
+    {
+        return View();
+    }
+        //Zadanie 2 dodaj do kalkulatora: operator pow(podnosi x do potegi y), funckje sin ktora oblicza sin(x), y jest zbedne
+      
+    public IActionResult Calculator(Operator? op, double? x, double? y)
+    {
+        //var op = Request.Query["op"];
+       // var x = double.Parse(Request.Query["x"]);
+        //var y = double.Parse(Request.Query["y"]);
+        if (x is null)
+        {
+            ViewBag.ErrorMessage = "Niepoprawny format liczby w parametrze x";
+            return View("CalculatorError");
+        }
+
+        if (op is null)
+        {
+            ViewBag.ErrorMessage = "Nieznany operator!";
+            return View("CalculatorError");
+        }
+
+        
+        if (op != Operator.Sin && y is null)
+        {
+            ViewBag.ErrorMessage = "Niepoprawny format liczby w parametrze y";
+            return View("CalculatorError");
+        }
+
+        switch (op)
+        {
+            case Operator.Add:
+                ViewBag.Result = x + y;
+                break;
+            case Operator.Sub:
+                ViewBag.Result = x - y;
+                break;
+            case Operator.Mul:
+                ViewBag.Result = x * y;
+                break;
+            case Operator.Div:
+                ViewBag.Result = x / y;
+                break;
+            case Operator.Pow:
+                ViewBag.Result = Math.Pow((double)x, (double)y);
+                break;
+            case Operator.Sin:
+                ViewBag.Result = Math.Sin((Math.PI / 180) * (double)x);
+                break;
+            
+                
+            
+        }
+
+        return View();
+    }
+
     public IActionResult Index()
     {
         return View();
@@ -28,4 +86,9 @@ public class HomeController : Controller
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
     }
+}
+
+public enum Operator
+{
+    Add, Sub, Mul, Div, Pow, Sin
 }
