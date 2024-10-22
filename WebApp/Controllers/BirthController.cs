b1984bce383251f6b75b83f2ce3d189e62d9bee5
kacper.profic <kacper.profic@microsoft.wsei.edu.pl>
2024-10-22 17:12:16 +0200

    123

--- WebApp/Controllers/BirthController.cs
+++ WebApp/Controllers/BirthController.cs
@@ -1,12 +1,26 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
 
 namespace WebApp.Controllers;
 
 public class BirthController : Controller
 {
     // GET
-    public IActionResult Index()
+    public IActionResult Form()
     {
-        return View();
+        return View(new Birth());
+    }
+    
+    [HttpPost]
+    public IActionResult Result(Birth model)
+    {
+      
+        if (!model.IsValid())
+        {
+            return View("Error");
+        }
+        var age = model.CalculateAge();
+        model.Message = $"Cześć {model.Name}, masz {age} lat(a)."; 
+        return View(model);
     }
 }
--- WebApp/Models/Birth.cs
+++ WebApp/Models/Birth.cs
@@ -1,25 +1,26 @@
 namespace WebApp.Models;
 
 
-    public class Birth
+public class Birth
+{
+    public string Name { get; set; }
+    public DateTime? BirthDate { get; set; }
+    public string Message { get; set; }
+
+    public bool IsValid()
     {
-        public string Name { get; set; }
-        public DateTime? BirthDate { get; set; }
-        public string Message { get; set; }
-
-        public bool IsValid()
-        {
-            return !string.IsNullOrWhiteSpace(Name) && BirthDate.HasValue && BirthDate.Value < DateTime.Now;
-        }
-
-        public int CalculateAge()
-        {
-            if (!BirthDate.HasValue)
-                return 0;
-
-            var age = DateTime.Now.Year - BirthDate.Value.Year;
-            if (DateTime.Now < BirthDate.Value.AddYears(age)) age--;
-
-            return age;
-        }
+        return !string.IsNullOrWhiteSpace(Name) && BirthDate.HasValue && BirthDate.Value < DateTime.Now;
     }
+
+    public int CalculateAge()
+    {
+        if (!BirthDate.HasValue)
+            return 0;
+
+        var age = DateTime.Now.Year - BirthDate.Value.Year;
+        if (DateTime.Now < BirthDate.Value.AddYears(age)) age--;
+
+        return age;
+    }
+}
+    
