ef275d382497ce634140dd3ed7d5dd7fd0920f16
kacper.profic <kacper.profic@microsoft.wsei.edu.pl>
2024-10-22 18:21:20 +0200

    3312

---
+++ WebApp/Controllers/ContactController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers;
+
+public class ContactController : Controller
+{
+    private static Dictionary<int, ContactModel> _contacts = new()
+    {
+        {
+            1,
+            new ContactModel()
+            {
+                Id = 1,
+                FirstName = "Adam",
+                LastName = "Abecki",
+                Email = "adam@wsei.edu.pl",
+                PhoneNumber = "222 333 222",
+                BirthDate = new DateOnly(2000,10,10)
+            }
+            
+        },
+        {
+            2,
+            new ContactModel()
+            {
+                Id = 2,
+                FirstName = "Ewa",
+                LastName = "Sala",
+                Email = "ewa@wsei.edu.pl",
+                PhoneNumber = "212 343 222",
+                BirthDate = new DateOnly(2002, 03, 01)
+            }
+            
+        },
+
+    };
+
+    private static int currentId = 3;
+    // GET // Lista kontaków, przycisk dodawania kontaktu
+    public IActionResult Index()
+    {
+        return View(_contacts);
+    }
+    // formularz dodawania kontaktu
+    public IActionResult Add()
+    {
+        return View();
+    }
+    // odebranie danych z formularza, walidacja i dodanie kontaktu do kolekcji
+    [HttpPost]
+    public IActionResult Add(ContactModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View(model);
+        }
+
+        model.Id = ++currentId;
+        _contacts.Add(model.Id, model);
+        // dodanie modelu do kolekcji
+        return View("Index", _contacts);
+    }
+
+    public IActionResult Delete(int id)
+    {
+        _contacts.Remove(id);
+        return View("Index", _contacts);
+    }
+}
---
+++ WebApp/Models/ContactModel.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.Models;
+
+public class ContactModel
+{
+    [HiddenInput]
+    public int Id { get; set; }
+
+    [Required]
+    [MaxLength(length: 20, ErrorMessage = "Imię nie może być dłuższe niż 20 znaków")]
+    [MinLength(length: 2, ErrorMessage = "Imię musi mieć co najmniej 2 znaki!")]
+    public string FirstName { get; set; }
+    
+    [Required]
+    [MaxLength(length: 50, ErrorMessage = "Nazwiwsko nie może być dłuższe niż 20 znaków")]
+    [MinLength(length: 2, ErrorMessage = "Nazwisko musi mieć co najmniej 2 znaki!")]
+    public string LastName { get; set; }
+
+    [EmailAddress]
+    public string Email { get; set; }
+    
+    [Phone]
+    [RegularExpression(pattern:"\\d{3} \\d{3} \\d{3}", ErrorMessage = "Wpisz numer wg wzoru xxx-xxx-xxx")]
+    public string PhoneNumber { get; set; }
+
+    [DataType(DataType.Date)]
+    public DateOnly BirthDate { get; set; }
+}
